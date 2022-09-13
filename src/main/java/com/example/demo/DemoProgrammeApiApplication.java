75b9137e96cd46520ebf00bd04f625ed99cb5018
Naresh Kadava <nareshkadava@gmail.com>
2022-09-13 21:29:25 +0530

    crating the new demo project

---
+++ src/main/java/com/example/demo/DemoProgrammeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProgrammeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProgrammeApiApplication.class, args);
+	}
+
+}
